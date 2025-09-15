f816a0348ee26384f01c90b94d0db9c1b65eec11
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2025-09-15 22:19:22 +0530

    Add files via upload

---
+++ Download - Sample Database.sql
@@ -0,0 +1,19 @@
+/*
+-- Adventureworks database 
+      https://github.com/Microsoft/sql-server-samples/releases/tag/adventureworks
+      https://github.com/microsoft/sql-server-samples/releases/tag/adventureworks
+
+-- world wide importers database
+      https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+
+
+
+
+
+
+
+
+
+
+
+*/

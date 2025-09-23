734403067ea1f0e6eede8724c565f961975ddb92
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2025-09-23 13:52:07 +0530

    Update Download - Sample Database.sql

--- Download - Sample Database.sql
+++ Download - Sample Database.sql
@@ -10,7 +10,9 @@
 -- world wide importers database
       https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
 
-
+-- Kendra Little: Sample database - BabyNames
+      https://github.com/LitKnd/BabbyNames/releases/tag/v1.1
+      https://github.com/LitKnd/BabbyNames/files/784243/BabbyNames.bak.zip
 
 
 

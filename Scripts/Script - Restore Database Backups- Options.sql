821dbabf95717b06883c3721f6517e3da74a3864
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2025-10-13 16:53:13 +0530

    Updated Script

--- Scripts/Script - Restore Database Backups- Options.sql
+++ Scripts/Script - Restore Database Backups- Options.sql
@@ -1,6 +1,19 @@
-/*
-restore filelistonly from 
-disk = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\AdventureWorks2019\AdventureWorks2019_backup_2024_03_12_160002_0410057..bak'
+
+-- Script - Database - Restore history
+
+SELECT 
+* 
+FROM 
+msdb.dbo.restorehistory 
+WHERE 
+destination_database_name = '%dba%'
+order by 
+restore_date DESC;
+
+
+
+/* -- Check DB file details
+restore filelistonly from disk = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\AdventureWorks2019\AdventureWorks2019_backup_2024_03_12_160002_0410057..bak'
 
 */
 

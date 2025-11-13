7e519e218d615ccd579db4606257d9e446fc0dc5
Pradeep Prakash <pradeep_prakash@outlook.com>
2025-11-13 18:35:49 +0530

    added more points

--- Demo/Demo Dynamic SQL.sql
+++ Demo/Demo Dynamic SQL.sql
@@ -21,5 +21,29 @@ When to Use
 . Variables for server, database, or object names
 
 "Catch-All" Queries: A single query that tries to account for all options available.
+----------------------------------------------------------------------------------------
+        WHERE (VIN = @vin OR @vin IS NULL)
+        AND (BaseModelID = @BaseModelID OR @BaseModelID IS NULL)
+        AND (PackageID = @PackageID OR @PackageID IS NULL)
+        AND (TrueCost = @TrueCost OR @TrueCost IS NULL)
+        AND (InvoicePrice = @InvoicePrice OR @InvoicePrice IS NULL)
 
+End User Customized Queries
+----------------------------------------------------------------------------------------
+Application has screens that display different data based on what different clients screen.
+
+choose.
+
+Dealership A wants:
+....................
+VIN
+True Cost
+Invoice Price
+
+Dealership B wants:
+...................
+VIN
+Base Model Name
+Package Name
+MSRP
 

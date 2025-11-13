6d019de590ccc1f938941f6a731f7af7c876c215
Pradeep Prakash <pradeep_prakash@outlook.com>
2025-11-13 18:14:28 +0530

    added agenda points

--- Demo/Demo Dynamic SQL.sql
+++ Demo/Demo Dynamic SQL.sql
@@ -4,9 +4,22 @@ Deborah Melkin: Mastering Dynamic SQL: https://www.youtube.com/watch?v=da8VAhwej
 
 */
 
+Q: What is Dynamic SQL?
+A: SQL statements that are created ad hoc and executed.
 
+Agenda
+----------
+· Use Cases
+· Security
+· Syntax
+· Demos
+· Tips & Tricks
 
+When to Use
+. "Catch All" Queries
+. End User Customized Queries
+. Variables for server, database, or object names
 
-
+"Catch-All" Queries: A single query that tries to account for all options available.
 
 
